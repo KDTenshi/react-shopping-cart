dcbcd75f7de3ed838d729fc3de6df3faa6a1916d
Артур Мыльников <kuraidatenshidesu@gmail.com>
2024-12-28 17:10:21 +0300

    Created Header

--- src/app/layout/App.tsx
+++ src/app/layout/App.tsx
@@ -1,7 +1,13 @@
 import { FC } from "react";
+import "../style/App.css";
+import Header from "../../components/Header/Header";
 
 const App: FC = () => {
-  return <main></main>;
+  return (
+    <main className="App">
+      <Header />
+    </main>
+  );
 };
 
 export default App;
