8c6e75c7b7d14ba64d1a854a9dbd2e68645aba3c
Артур Мыльников <kuraidatenshidesu@gmail.com>
2024-12-28 18:45:37 +0300

    Added functionality to Cart

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,18 @@
 import { FC } from "react";
 import style from "./Header.module.css";
+import { useAppDispatch } from "../../app/store/appStore";
+import { showCart } from "../../shared/model/cartSLice";
 
 const Header: FC = () => {
+  const dispatch = useAppDispatch();
+
   return (
     <header className={style.Header}>
       <h1 className={style.Title}>Cart App</h1>
       <div className={style.Buttons}>
-        <button className={style.Button}>Cart</button>
+        <button className={style.Button} onClick={() => dispatch(showCart())}>
+          Cart
+        </button>
       </div>
     </header>
   );
